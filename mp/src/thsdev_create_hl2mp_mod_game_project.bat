f4839e8d85f23d889f004ea92b5106f48dcda905
Ivan Suvorov <56027883+THS2121966@users.noreply.github.com>
2024-02-03 01:49:37 +0600

    [THS] Move Buildprojects Startup Scripts.

---
+++ mp/src/thsdev_create_hl2mp_mod_game_project.bat
@@ -0,0 +1,2 @@
+devtools\bin\vpc.exe /THS_HL2MP_MODIFIED +game /mksln thsdev_hl2mp.sln
+pause
---
+++ mp/src/thsdev_createallprojects.bat
@@ -0,0 +1,2 @@
+devtools\bin\vpc.exe /THS_HL2MP_MODIFIED /THS_EPISODIC_MODIFIED +everything /mksln thsdev_main_project.sln
+pause
