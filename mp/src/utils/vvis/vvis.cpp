22a47849d9631c0893bf1c62bdd8727a106166c0
Ivan Suvorov <avatar2329@gmail.com>
2024-02-05 19:52:49 +0600

    Update vvis.cpp

--- mp/src/utils/vvis/vvis.cpp
+++ mp/src/utils/vvis/vvis.cpp
@@ -1076,7 +1076,7 @@ int RunVVis( int argc, char **argv )
 	double		start, end;
 
 
-	Msg( "Valve Software - vvis.exe (%s)\n", __DATE__ );
+	Msg( "Valve Software and THS inc 2024 - vvis.exe (%s)\n", __DATE__ );
 
 	verbose = false;
 
