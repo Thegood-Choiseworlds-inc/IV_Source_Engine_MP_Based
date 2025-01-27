fc6db6e3c599641fb5376f3ad884be57847bc194
Ivan Suvorov <56027883+THS2121966@users.noreply.github.com>
2025-01-28 02:23:31 +0600

    Update c_env_material_global_parms.cpp

--- mp/src/game/client/c_env_material_global_parms.cpp
+++ mp/src/game/client/c_env_material_global_parms.cpp
@@ -13,11 +13,11 @@
 static bool g_mat_force_phong = false;
 static bool g_mat_force_anisotropic = false;
 
-void IV_Force_Reload_All_Materials()
+/*void IV_Force_Reload_All_Materials()
 {
 	materials->UncacheAllMaterials();
 	materials->CacheUsedMaterials();
-}
+}*/
 
 
 //------------------------------------------------------------------------------
@@ -49,13 +49,15 @@ END_RECV_TABLE()
 C_MatGlobalParmsControl::C_MatGlobalParmsControl()
 {
 	//IV Note: Force Reload All Materials for Revent Changes!!!
-	if (g_mat_force_phong || g_mat_force_anisotropic)
+	/*if (g_mat_force_phong || g_mat_force_anisotropic)
 	{
 		g_mat_force_phong = false;
 		g_mat_force_anisotropic = false;
 		Warning("Force Revent Back Material Global Parms!!!\n");
 		IV_Force_Reload_All_Materials();
-	}
+	}*/
+
+	Warning("Material Global Parms Logic is Unfinished Yet and Disabled by Default!!! Use Material Parms Override in Console Insead of That Entity!!!");
 }
 
 //------------------------------------------------------------------------------
@@ -65,7 +67,7 @@ C_MatGlobalParmsControl::C_MatGlobalParmsControl()
 //------------------------------------------------------------------------------
 void C_MatGlobalParmsControl::OnDataChanged(DataUpdateType_t updateType)
 {
-	ConVarRef temp_convar_force_lightmapped_phong("ivdev_engine_force_lightmapped_phong");
+	/*ConVarRef temp_convar_force_lightmapped_phong("ivdev_engine_force_lightmapped_phong");
 	ConVarRef temp_convar_force_envmap_anisotopy("ivdev_engine_force_envmap_anisotropy");
 
 	if (!temp_convar_force_lightmapped_phong.IsValid() || !temp_convar_force_envmap_anisotopy.IsValid())
@@ -86,7 +88,9 @@ void C_MatGlobalParmsControl::OnDataChanged(DataUpdateType_t updateType)
 	Warning("Force Applying Material Parms for Remake Map!!!\n");
 
 	//IV Note: Force Reload All Materials for Apply Changes!!!
-	IV_Force_Reload_All_Materials();
+	IV_Force_Reload_All_Materials();*/
+
+	Warning("[MATERIALS DATA CHANGE ABORTED!!!] Material Global Parms Logic is Unfinished Yet and Disabled by Default!!! Use Material Parms Override in Console Insead of That Entity!!!");
 }
 
 //------------------------------------------------------------------------------
